71a7eb27bbaa6d39712fd0fe366ae8c28af8bbd8
13clarissa <clarissa.corea@unah.hn>
2022-10-21 20:22:09 -0600

    Avances
    Nuevos avances del proyecto

---
+++ Proyecto_IIUnidad/Datos/CadenaConexion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public static class CadenaConexion
+    {
+        public static string Cadena = "Data Source=localhost; Initial Catalo=mifactura; User ID=root; Password=Clary2000";
+
+
+    }
+}
